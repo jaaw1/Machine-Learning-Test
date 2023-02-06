a753b2dd5db5d4a3dd908c9bd5b3b78568add1ad
jaaw1 <jaani.nurisalo@edu.turkuamk.fi>
2023-02-06 21:51:59 +0200

    Corrected percentages.

--- similarity_of_columns.py
+++ similarity_of_columns.py
@@ -6,7 +6,7 @@ def similarity_of_columns(table_1, table_2):
     percentages_table = [[0.0 for x in range(len(table_1))] for y in range(len(table_2))]
     for i in range(len(table_1)):
         for j in range(len(table_2)):
-            percentage = around(100 * (abs(average(table_1[:, i] - table_2[:, j]))), 2)
+            percentage = 100 - around(100 * (average(abs(table_1[:, i] - table_2[:, j]))), 2)
             percentages_table[i][j] = percentage
 
     return percentages_table
